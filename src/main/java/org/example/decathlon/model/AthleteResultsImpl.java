1c7b9a34a8f2b3a47abf2b4a495b564a56473081
Konstantin Degtyaryov <konstantin.degtyaryov@gmail.com>
2021-02-18 00:14:15 +0600

    Import reorganized.

--- src/main/java/org/example/decathlon/model/AthleteResultsImpl.java
+++ src/main/java/org/example/decathlon/model/AthleteResultsImpl.java
@@ -1,6 +1,9 @@
 package org.example.decathlon.model;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.BiFunction;
 
 /**
