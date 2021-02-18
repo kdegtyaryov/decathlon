09b2cc753567ebc58d4d4f7e4d8289571583f73d
Konstantin Degtyaryov <konstantin.degtyaryov@gmail.com>
2021-02-18 12:08:20 +0600

    pom fixed.

--- src/main/java/org/example/decathlon/model/input/AthleteResultsReader.java
+++ src/main/java/org/example/decathlon/model/input/AthleteResultsReader.java
@@ -1,12 +1,7 @@
 package org.example.decathlon.model.input;
 
 import org.example.decathlon.model.AthleteResults;
-import org.example.decathlon.model.AthleteResultsImpl;
-import org.example.decathlon.model.utils.Convertor;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * Represent a reader for loading event results from a file.
